68785e839197b0482a4bd72d665c0a9152446ea1
Kirsten Smith <kvmadgirl@hotmail.com>
2012-03-12 02:47:55 +0000

    Sorting out routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 AWADNewsApp::Application.routes.draw do
 
+  root :to => 'headlines#index'
 
   resources :headlines do
       resources :comments
@@ -55,11 +56,11 @@ AWADNewsApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'headlines#index'
+
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+   match ':controller(/:action(/:id))(.:format)'
 end
